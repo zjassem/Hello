2cbd37b12cfafb1b91aa2c06d3ce51b114528e0d
zjassem <130488241+zjassem@users.noreply.github.com>
2024-02-08 15:34:59 -0500

    Update Hello.java

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
-    public static void main(String[] args) throws Exception {
-        for(int i=1; i<=11;i++){
+    public static void main(String[] args) {
+        for(int i=1;i<=11;i++) {
             System.out.println("Hello World..."+i);
         }
     }
