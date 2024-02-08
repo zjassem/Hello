a102aa0c75bfd115a69be0b248e0b18f5f8dc687
Zainab Jassem <zjassem@umich.edu>
2024-02-08 14:45:58 -0500

    new commit yayy

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+    for(int i=1;i<=11;i++) {
+    System.out.println("Hello World..."+i);
     }
-}
+    }
+    }
